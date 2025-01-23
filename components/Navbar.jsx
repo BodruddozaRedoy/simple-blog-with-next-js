042d9dca51fe104c063809e1b73183ee07a63a3f
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2025-01-23 14:32:55 +0600

    implemented condition based buttons

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,27 +1,41 @@
-import React from 'react'
-import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
-import {LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
-import Link from 'next/link';
+import React from "react";
+import {
+  RegisterLink,
+  LoginLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import Link from "next/link";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const { isAuthenticated } = getKindeServerSession();
+  const isUserAuthenticated = await isAuthenticated();
+  console.log(isUserAuthenticated);
+
   return (
-    <div className='flex justify-between items-center bg-slate-200 text-black px-10 py-5'>
+    <div className="flex justify-between items-center bg-slate-200 text-black px-10 py-5">
       <div>Simple Blog</div>
-      <div className='flex gap-3 items-center list-none'>
-        <Link href={'/'}>Home</Link>
-        <Link href={'/profile'}>Profile</Link>
+      <div className="flex gap-3 items-center list-none">
+        <Link href={"/"}>Home</Link>
+        <Link href={"/profile"}>Profile</Link>
       </div>
-        <div>
-            <LoginLink>
-            <button className='btn py-3 px-5 bg-blue-500 rounded-lg text-white font-semibold mr-5'>Login</button>
-            </LoginLink>
-            <LogoutLink>
-            <button className='btn py-3 px-5 bg-blue-500 rounded-lg text-white font-semibold'>Logout</button>
-            </LogoutLink>
-            
-        </div>
+      <div>
+        {isUserAuthenticated ? (
+          <LogoutLink>
+            <button className="btn py-3 px-5 bg-blue-500 rounded-lg text-white font-semibold">
+              Logout
+            </button>
+          </LogoutLink>
+        ) : (
+          <LoginLink>
+            <button className="btn py-3 px-5 bg-blue-500 rounded-lg text-white font-semibold mr-5">
+              Login
+            </button>
+          </LoginLink>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
